ab4cbf36fed582da1940a8fe99ba15cb61e825d7
Kevin Elliott <kevin@welikeinc.com>
2016-10-28 00:54:21 -0700

    Stylistic improvements.

--- app/models/order.rb
+++ app/models/order.rb
@@ -9,6 +9,15 @@ class Order < ApplicationRecord
     order_id.present? ? "#{order_id.slice(0..-5)}****" : nil
   end
 
+  def payment_status_class
+    case payment_status
+    when 'Pay Confirmed' then 'payment-status-pay-confirmed'
+    when 'Pending' then 'payment-status-pending'
+    else
+      'payment-status-unknown'
+    end
+  end
+
   def shipping_status_class
     case shipping_status.downcase.to_sym
     when :pending then 'shipping-status-pending'
