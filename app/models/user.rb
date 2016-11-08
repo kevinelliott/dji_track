271724160fdaadafb1eb1a593f9880876f56ac2d
Kevin Elliott <kevin@welikeinc.com>
2016-11-08 09:46:20 -0800

    Sign Up Form Enhancement
    * add the username to the Sign Up form
    * add a validation to enforce the username

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
-  enum role: [:user, :vip, :admin]
-  after_initialize :set_default_role, :if => :new_record?
+  enum role: [:user, :vip, :contributor, :editor, :admin]
+  after_initialize :set_default_role, if: :new_record?
+
+  validates :username, presence: true, uniqueness: true
 
   def set_default_role
     self.role ||= :user
