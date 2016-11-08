c25053b63d0f8980ca2677e1db86386490d2a678
Kevin Elliott <kevin@welikeinc.com>
2016-11-08 05:24:05 -0800

    Tiny little change to the Orders form.

--- app/models/order_state_log.rb
+++ app/models/order_state_log.rb
@@ -16,6 +16,7 @@ class OrderStateLog < ApplicationRecord
     def notify_slack(order, column, from, to)
       if column == 'shipping_status' && to.downcase == 'shipped'
         message = "Order #{order.masked_order_id} from #{order.order_time} to #{order.shipping_country.upcase} was just shipped."
+
         slack = Slack::Web::Client.new
         slack.chat_postMessage(
           channel: '#mavic-shipping-status',
