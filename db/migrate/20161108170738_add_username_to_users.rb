84bf9dd824fd5ee77005e780afb2abea9e63569d
Kevin Elliott <kevin@welikeinc.com>
2016-11-08 09:29:07 -0800

    Set the null constraint on username correctly.
    First we want to add the new column, generate a dynamic username for
    any existing users (shouldn’t be more than one or two at this point)
    and then add the null constraint.

--- db/migrate/20161108170738_add_username_to_users.rb
+++ db/migrate/20161108170738_add_username_to_users.rb
@@ -1,6 +1,12 @@
 class AddUsernameToUsers < ActiveRecord::Migration[5.0]
   def change
-    add_column :users, :username, :string, null: false
+    add_column :users, :username, :string
+
+    User.all.each do |u|
+      u.update(username: ('a'..'z').to_a.shuffle[0,8].join)
+    end
+    
+    change_column_null :users, :username, false
     add_index :users, :username, unique: true
   end
 end
