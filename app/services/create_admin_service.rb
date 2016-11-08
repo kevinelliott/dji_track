d2bc65fb31eb76784e5502641ff75bb3e0ce523a
Kevin Elliott <kevin@welikeinc.com>
2016-11-08 09:16:21 -0800

    Add username to users, and update the CreateAdminService to support it.

--- app/services/create_admin_service.rb
+++ app/services/create_admin_service.rb
@@ -3,6 +3,7 @@ class CreateAdminService
     user = User.find_or_create_by!(email: Rails.application.secrets.admin_email) do |user|
         user.password = Rails.application.secrets.admin_password
         user.password_confirmation = Rails.application.secrets.admin_password
+        user.username = Rails.application.secrets.admin_username
         user.confirm!
         user.admin!
       end
