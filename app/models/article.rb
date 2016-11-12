37c6511feca64b14513852547414074800df2b3b
Kevin Elliott <kevin@welikeinc.com>
2016-11-12 03:35:59 -0800

    Add publish! method to article and also notify Slack.

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,6 +2,22 @@ class Article < ApplicationRecord
 
   belongs_to :user
 
+  def notify_slack
+    message = "#{order.merchant.common_name}: Order #{order.masked_order_id} for #{order.product.name} on #{order.order_time.presence || 'an unknown Order Time'} to #{(order.shipping_country.presence || 'UNKNOWN').upcase} was just shipped."
+
+    slack = Slack::Web::Client.new
+    slack.chat_postMessage(
+      channel: '#general',
+      text: message,
+      as_user: true
+    )
+  end
+
+  def publish!
+    self.update(status: 'published', published_at: Time.current)
+    notify_slack
+  end
+
   def to_param
     "#{id}-#{title.downcase.gsub(' ', '-').dasherize}"
   end
