0e2fcc6de652ce14007837296fceb145bbd1a8a3
Kevin Elliott <kevin@welikeinc.com>
2016-11-12 03:37:09 -0800

    Let's make the message for the Slack for new articles correct.

--- app/models/article.rb
+++ app/models/article.rb
@@ -3,7 +3,7 @@ class Article < ApplicationRecord
   belongs_to :user
 
   def notify_slack
-    message = "#{order.merchant.common_name}: Order #{order.masked_order_id} for #{order.product.name} on #{order.order_time.presence || 'an unknown Order Time'} to #{(order.shipping_country.presence || 'UNKNOWN').upcase} was just shipped."
+    message = "A new DroneHome article has been published: #{link_to title, articles_path}"
 
     slack = Slack::Web::Client.new
     slack.chat_postMessage(
