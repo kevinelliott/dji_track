94f6c6bd8290f84be6d6ab0fd10ee9d41acbd01a
Kevin Elliott <kevin@welikeinc.com>
2016-11-29 10:29:15 -0800

    Hash merge fix.

--- app/services/order_info_service.rb
+++ app/services/order_info_service.rb
@@ -11,15 +11,42 @@ class OrderInfoService
       delivered_count = Order.delivered.count
       enroute_count   = Order.enroute.count
 
-      default_counts.merge { delivered: delivered_count, enroute: enroute_count }
+      default_counts.merge(delivered: delivered_count, enroute: enroute_count)
     when :shipping_status
       not_shipped_count = Order.not_shipped.count
       shipped_count     = Order.shipped.count
 
-      default_counts.merge { not_shipped: not_shipped_count, shipped: shipped_count }
+      default_counts.merge(not_shipped: not_shipped_count, shipped: shipped_count)
     else
       default_counts
     end
+  end
+
+  def merchant_status_validation_request(order)
+    if order.merchant.common_name == 'DJI'
+      # Retrieve Order Tracking page to get captcha support
+      tracking_page = DJI::OrderTracking.tracking_page
+
+      # Store DJI state, set expiration, captcha image in database
+      request = order.validation_requests.create(...)
+
+      request
+    end
+  end
+
+  def merchant_status(validation_request, params = {})
+    order = validation_request.order
+
+    # Send captcha details along with form
+    options = {
+      order_number: order.order_id,
+      phone_tail: order.phone_tail,
+      captcha: params[:captcha],
+      session: validation_request.session_id,
+      debug: false
+    }
+
+    DJI::OrderTracking.tracking_details(options)
   end
 
 end
